94f3ef75461c15472d91ed04d97824a75ddd71a7
Venky <vvodyaram@ctepl.com>
2023-11-17 23:00:34 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  baseUrl:'https://wpservices.wearablesclinicaltrials.com/wearables',
+  baseUrl2:'',
+  grantType: 'password',
+  clientId: 'adminClientId',
+  clientSecret: '$CLIENT_SECRET',
+  tokenString: 'userToken',
+  userId: 'userId',
+  refreshTokenString: 'userRefreshToken'
+};
